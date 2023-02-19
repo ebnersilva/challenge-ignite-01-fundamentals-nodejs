9cad4208f273910bf9a3b2697a0870330adde585
Ebner Silva <ebnersilva@MacBook-Pro-de-Ebner.local>
2023-02-19 14:55:32 -0300

    refactor: remove unnecessary route

--- src/routes.js
+++ src/routes.js
@@ -142,27 +142,5 @@ export const routes = [
         .writeHead(204)
         .end();
     }
-  },
-  {
-    method: 'POST',
-    path: buildRoutePath('/import'),
-    handler: (req, res) => {
-      const { title, description } = req.body;
-
-      const task = {
-        id: randomUUID(),
-        title,
-        description,
-        completed_at: null,
-        created_at: new Date(),
-        updated_at: new Date()
-      }
-
-      database.insert('tasks', task)
-
-      return res
-        .writeHead(201)
-        .end()
-    }
-  },
+  }
 ]
